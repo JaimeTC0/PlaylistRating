ade1e4bd7a44adadbf8d546a7c686e3c190c2340
JaimeTC0 <ja386078@ucf.edu>
2026-03-31 18:02:03 -0400

    Added basic mainpage information

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,12 +1,49 @@
-
+import {useState, useEffect} from "react";
 
 function MainPage(){
 
+    const [artists, setArtists] = useState([]);
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/allArtists")
+        .then(res => res.json())
+        .then(artistData => setArtists(artistData))
+        .catch(err => console.log(err));
+    }, []);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/allAlbums")
+        .then(res => res.json())
+        .then(albumData => setAlbums(albumData))
+        .catch(err => console.log(err));
+    }, []);
 
     return(
         <>
             <p>Welcome Home</p>
-            // Add more content here as needed
+            
+            <h1>Artists</h1>
+
+            <div className="MainDiv">
+            {artists.map((artist, index) => (
+                <div key={index} className="List">
+                    <img src="https://placehold.co/150x150" className="AlbumCover"/>
+                    <p>{artist.ArtistName}</p>
+                </div>
+            ))}
+            </div>
+
+            <h1>Recent Albums</h1>
+
+            <div className="MainDiv">
+            {albums.map((album, index) => (
+                <div key={index} className="List">
+                    <img src="https://placehold.co/150x150" className="AlbumCover"/>
+                    <p>{album.AlbumName}</p>
+                </div>
+            ))}
+            </div>
         </>
     )
 }
