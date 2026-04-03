e5ece9e68f7733586b7efa891c0673801c6fd41c
TavisYearwood1 <ta313579@ucf.edu>
2026-04-03 14:29:07 -0400

    Update auth.js

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -1,1 +1,24 @@
+const jwt = require("jsonwebtoken");
 
+const auth = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const verified = jwt.verify(token, "SECRET_KEY");
+    req.user = verified;
+    next();
+  } catch {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
+
+const adminOnly = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Admin only" });
+  }
+  next();
+};
+
+module.exports = { auth, adminOnly };
