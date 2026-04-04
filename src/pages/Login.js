55e491e3d5b155e6385f68695bba77fb0392bed5
TavisYearwood1 <ta313579@ucf.edu>
2026-04-04 09:56:31 -0400

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,24 @@
 import { useState } from "react";
 import axios from "axios";
 
+import axios from "axios";
+
+const API = "http://localhost:5176/api";
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  try {
+    const res = await axios.post(`${API}/auth/login`, form);
+
+    localStorage.setItem("token", res.data.token);
+    localStorage.setItem("role", res.data.role);
+
+    window.location.href = "/";
+  } catch (err) {
+    console.error(err);
+  }
+};
 export default function Login() {
   const [form, setForm] = useState({
     email: "",
